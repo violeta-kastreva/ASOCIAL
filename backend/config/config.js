fc1a85e13eea43a867fd7b8c36cb81f724da5d4f
ElitsaY <eyotkova@gmail.com>
2025-03-15 15:47:35 +0200

    Modify config

--- backend/config/config.js
+++ backend/config/config.js
@@ -5,8 +5,8 @@ module.exports = {
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
             host: process.env.DB_HOST,
-            dialect: 'postgres',
             port: process.env.DB_PORT,
+            dialect: 'postgres',
             logging: false,
         },
         test: {
