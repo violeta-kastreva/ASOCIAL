c0b1df956b49fcdf974ac06d9cff8cdca0ce6160
aleksanderdimitrov <snowtromgs@gmail.com>
2025-03-15 22:50:31 +0200

    quick fix

--- backend2/app.js
+++ backend2/app.js
@@ -3,15 +3,12 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
-// Load env vars
 dotenv.config();
 
-// Connect to database
 connectDB();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
